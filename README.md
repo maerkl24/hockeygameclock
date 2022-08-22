7b991d765c859947b53d5a21c6d44ec5fa28642e
maerkl24 <kilianmaerkl@web.de>
2022-08-22 18:28:49 +0200

    Fix paths and formating

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ deactivate
 yapf --style google -i -r hockeyclock
 
 # Lint Python code
-pylint --rcfile pylintrc hockeyclock
+pylint --rcfile .pylintrc hockeyclock
 
 # Build the docs
 cd docs
--- hockeyclock/backend/player.py
+++ hockeyclock/backend/player.py
@@ -2,7 +2,6 @@
 """
 from typing import Optional, Literal
 
-
 # Position for Player
 Position = Literal['C', 'A', 'G', 'F']
 
@@ -10,8 +9,12 @@ Position = Literal['C', 'A', 'G', 'F']
 class Player():
     """Class representing a player."""
 
-    def __init__(self, number: int, position: Optional[Position] = None, forename: Optional[str] = None,
-                 surname: Optional[str] = None, pass_number: Optional[int] = None) -> None:
+    def __init__(self,
+                 number: int,
+                 position: Optional[Position] = None,
+                 forename: Optional[str] = None,
+                 surname: Optional[str] = None,
+                 pass_number: Optional[int] = None) -> None:
         self.number = number
         self.position = position
         self.forename = forename
